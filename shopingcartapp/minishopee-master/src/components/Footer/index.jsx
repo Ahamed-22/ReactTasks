abfebcef827bb5f7c9da8757060ab46068c6098f
Ahamed-22 <ahamedofficial22@gmail.com>
2024-07-08 11:35:37 +0530

    first commit

---
+++ shopingcartapp/minishopee-master/src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="py-5" style={{ backgroundColor : '#A9D39E' }}>
+      <div className="container">
+        <p className="m-0 text-center text-white">
+          Copyright &copy; Your Website 2023
+        </p>
+      </div>
+    </footer>
+  );
+}
